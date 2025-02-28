91e112aad35d0743ead66a0490b9df6d60a6b790
Jose Amadeo Diaz Diaz <jose.diaz@joedayz.pe>
2025-02-27 19:15:54 -0500

    Test para imp/exp data

--- pruebas-vite/src/data/heroes.js
+++ pruebas-vite/src/data/heroes.js
@@ -29,7 +29,4 @@ const heroes = [
 export const owners = ['DC','Marvel'];
 
 export default heroes;
-// export {
-//     heroes as default,
-//     owners
-// }
+
--- pruebas-vite/tests/base-pruebas/imp-exp.test.js
+++ pruebas-vite/tests/base-pruebas/imp-exp.test.js
@@ -1,5 +1,5 @@
 
-import { getHeroeById } from '../../src/base-pruebas/imp-exp';
+import { getHeroeById, getHeroesByOwner } from '../../src/base-pruebas/imp-exp';
 
 describe('Pruebas en el archivo imp-exp.test.js', () => {
 
@@ -16,6 +16,28 @@ describe('Pruebas en el archivo imp-exp.test.js', () => {
                     const heroe = getHeroeById(id);
                     console.log(heroe);
                     //expect(heroe).toEqual({ id:1 , name: 'Batman', owner: 'DC' });
-                    expect(heroe).toBe(undefined);
+                    expect(heroe).toBeFalsy();
+          });
+
+          test('getHeroesByOwner debe retornar un arreglo con los héroes de DC', () => {
+                    const owner = 'DC';
+                    const heroes = getHeroesByOwner(owner);
+                    
+                    expect( heroes.length ).toBe(3);
+                    expect ( heroes ).toEqual([
+                              { id:1 , name: 'Batman', owner: 'DC' },
+                              { id:3 , name: 'Superman', owner: 'DC' },
+                              { id:4 , name: 'Flash', owner: 'DC' }
+                    ]);
+
+                    expect( heroes ).toEqual( heroes.filter( (heroe) => heroe.owner === owner ));
+          });
+
+          test('getHeroesByOwner debe retornar un arreglo con los héroes de Marvel', () => {
+                    const owner = 'Marvel';
+                    const heroes = getHeroesByOwner(owner);
+                    
+                    expect( heroes.length ).toBe(2);
+                    expect( heroes ).toEqual( heroes.filter( (heroe) => heroe.owner === owner ));
           });
 });
